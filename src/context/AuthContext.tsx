caba13981b2b3e9fd36ba9c18e8b8324f8312c0f
wonieeVicky <hwfongfing@gmail.com>
2023-08-21 19:51:36 +0900

    feat: authContext 컴포넌트 구현(SessionProvider)

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,11 @@
+﻿'use client'; //next에서 context를 사용하기 위해서는 csr 렌더링으로 명시해야 함
+// context는 상태를 가지고 있기 때문에 서버컴포넌트는 상태에 접근 할 수 없음
+import { SessionProvider } from 'next-auth/react';
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function AuthContext({ children }: Props) {
+  return <SessionProvider>{children}</SessionProvider>;
+}
