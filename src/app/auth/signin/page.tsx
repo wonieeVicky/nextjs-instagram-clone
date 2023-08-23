5b8d4cc0a1b96f16346de0b883aa6b6aadeadb81
wonieeVicky <hwfongfing@gmail.com>
2023-08-23 23:26:32 +0900

    feat: signIn 페이지 레이아웃 구현

---
+++ src/app/auth/signin/page.tsx
@@ -0,0 +1,21 @@
+﻿'use client';
+import ColorButton from '@/components/ui/ColorButton';
+import { signIn } from 'next-auth/react';
+import { useSearchParams } from 'next/navigation';
+
+export default function SignInPage() {
+  const searchParams = useSearchParams();
+  return (
+    <div className="h-screen text-center pt-24">
+      <ColorButton
+        text="Sign In with Google"
+        size="lg"
+        onClick={() =>
+          signIn('google', {
+            callbackUrl: searchParams?.get('callbackUrl') || '/'
+          })
+        }
+      />
+    </div>
+  );
+}
