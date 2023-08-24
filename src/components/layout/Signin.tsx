26a62aabb924354c91ce4992d69b4c84c79e13c0
wonieeVicky <hwfongfing@gmail.com>
2023-08-25 00:49:52 +0900

    feat: csr signIn 컴포넌트 별도 분리

---
+++ src/components/layout/Signin.tsx
@@ -0,0 +1,24 @@
+﻿'use client';
+
+import { ClientSafeProvider, signIn } from 'next-auth/react';
+import ColorButton from '../ui/ColorButton';
+
+type Props = {
+  providers: Record<string, ClientSafeProvider>;
+  callbackUrl: string;
+};
+
+export default function Signin({ providers, callbackUrl }: Props) {
+  return (
+    <>
+      {Object.values(providers).map(({ name, id }) => (
+        <ColorButton
+          key={id}
+          text={`Sign In with ${name}`}
+          size="lg"
+          onClick={() => signIn(id, { callbackUrl })}
+        />
+      ))}
+    </>
+  );
+}
