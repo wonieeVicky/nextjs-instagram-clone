e93ae54a5e511d312aa6788261b3c895095ce210
wonieeVicky <hwfongfing@gmail.com>
2023-08-25 00:49:34 +0900

    fix: ColorButton 내 size 속성 관련 스타일 수정

--- src/components/ui/ColorButton.tsx
+++ src/components/ui/ColorButton.tsx
@@ -1,15 +1,19 @@
 ﻿type Props = {
   text: string;
   onClick: () => void;
-  size?: 'sm' | 'md' | 'lg';
+  size?: 'md' | 'lg';
 };
 
 export default function ColorButton({ text, onClick, size = 'md' }: Props) {
   return (
-    <div className="rounded-md bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-300 p-[0.15rem] inline-block">
+    <div
+      className={`rounded-md bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-300 inline-block ${
+        size === 'lg' ? 'p-[0.3rem]' : 'p-[0.15rem]'
+      }`}
+    >
       <button
-        className={`bg-white rounded-sm px-[0.3rem] hover:opacity-90 transition-opacity ${
-          size === 'lg' ? 'text-3xl p-[1rem] px-[1rem]' : 'text-bases'
+        className={`bg-white rounded-sm hover:opacity-90 transition-opacity ${
+          size === 'lg' ? 'p-4 text-2xl' : 'p-[0.3rem] text-base'
         }`}
         onClick={onClick}
       >
@@ -17,11 +21,4 @@ export default function ColorButton({ text, onClick, size = 'md' }: Props) {
       </button>
     </div>
   );
-}
-{
-  /* <button className="relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white focus:ring-4 focus:outline-none focus:ring-pink-200">
-  <span className="relative py-1 px-2 transition-all ease-in duration-75 bg-white rounded-md group-hover:bg-opacity-0">
-    Sign in
-  </span>
-</button>; */
 }
