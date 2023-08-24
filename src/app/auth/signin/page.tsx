f72475fdeb414768082a84e894feed21504d7b4d
wonieeVicky <hwfongfing@gmail.com>
2023-08-25 00:50:40 +0900

    fix: signin 페이지를 ssr 페이지로 변경(getServerSession, getProvider)

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,21 +1,29 @@
-﻿'use client';
-import ColorButton from '@/components/ui/ColorButton';
-import { signIn } from 'next-auth/react';
-import { useSearchParams } from 'next/navigation';
+﻿import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import Signin from '@/components/layout/Signin';
+import { getServerSession } from 'next-auth';
+import { getProviders } from 'next-auth/react';
+import { redirect } from 'next/navigation';
 
-export default function SignInPage() {
-  const searchParams = useSearchParams();
+type Props = {
+  searchParams: {
+    callbackUrl: string;
+  };
+};
+
+export default async function SignInPage({
+  searchParams: { callbackUrl }
+}: Props) {
+  const session = await getServerSession(authOptions);
+
+  if (session) {
+    redirect('/');
+  }
+
+  const providers = (await getProviders()) ?? {};
+
   return (
-    <div className="h-screen text-center pt-24">
-      <ColorButton
-        text="Sign In with Google"
-        size="lg"
-        onClick={() =>
-          signIn('google', {
-            callbackUrl: searchParams?.get('callbackUrl') || '/'
-          })
-        }
-      />
-    </div>
+    <section className="flex justify-center mt-[30%]">
+      <Signin providers={providers} callbackUrl={callbackUrl ?? '/'} />
+    </section>
   );
 }
