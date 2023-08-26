2f0a6718a0b19f84ddafff09ec71320786d2ff29
wonieeVicky <hwfongfing@gmail.com>
2023-08-26 23:41:50 +0900

    fix: ColorImage > Avatar

---
+++ src/components/ui/Avatar.tsx
@@ -0,0 +1,14 @@
+﻿type Props = { image?: string | null };
+
+export default function Avatar({ image }: Props) {
+  return (
+    <div className="w-9 h-9 rounded-full bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-300">
+      <img
+        className="rounded-full p-[0.1rem]"
+        alt="user profile"
+        src={image ?? undefined}
+        referrerPolicy="no-referrer"
+      />
+    </div>
+  );
+}
