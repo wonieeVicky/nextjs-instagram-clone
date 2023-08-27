c8e8fe980cdefedbb365fc9120ab964381600fac
wonieeVicky <hwfongfing@gmail.com>
2023-08-27 22:29:35 +0900

    chore: 미사용 코드 제거(getPosts)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,7 @@
+// import { getPosts } from '@/service/sanity';
 import { createClient } from 'next-sanity';
 
-export default function Home({ ...props }) {
+export default async function Home() {
+  // const posts = await getPosts();
   return <h1 className="text-gray-900"></h1>;
-}
-
-const client = createClient({
-  projectId: process.env.SANITY_PROJECT_ID,
-  dataset: process.env.SANITY_DATASET,
-  apiVersion: '2023-08-14',
-  useCdn: false
-});
-
-// get client from sanity
-export async function getStatucProps() {
-  const pet = await client.fetch(`*[_type == "pet"]`);
-  return {
-    props: { pet }
-  };
 }
