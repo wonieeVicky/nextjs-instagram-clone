f1c72a7a4d1dd1eb31c4a05cce3f7028e74a904f
wonieeVicky <hwfongfing@gmail.com>
2023-08-28 23:04:34 +0900

    feat: createClient, createUser sanity 함수 구현

---
+++ src/service/sanity.ts
@@ -0,0 +1,27 @@
+﻿import { createClient } from '@sanity/client';
+// Import using ESM URL imports in environments that supports it:
+// import {createClient} from 'https://esm.sh/@sanity/client'
+
+export const client = createClient({
+  projectId: process.env.SANITY_PROJECT_ID,
+  dataset: process.env.SANITY_DATASET,
+  apiVersion: '2023-08-27',
+  useCdn: true,
+  token: process.env.SANITY_TOKEN
+});
+
+type User = {
+  email: string;
+  image: string;
+  name: string;
+  username: string;
+  _type: 'user';
+  _id: string;
+};
+
+export async function createUser(user: User) {
+  const result = await client
+    .createIfNotExists(user)
+    .then((res) => console.log(res));
+  return result;
+}
