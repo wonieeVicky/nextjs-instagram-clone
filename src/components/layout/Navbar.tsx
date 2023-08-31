c927c3d1ddc5282a51759672ece499cb7106f11a
wonieeVicky <hwfongfing@gmail.com>
2023-08-31 21:49:32 +0900

    feat: Avatar 기능 일부 수정

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -61,7 +61,7 @@ export default function Navbar() {
           {user && (
             <li>
               <Link href={`/user/${user.username}`}>
-                <Avatar image={user.image} />
+                <Avatar image={user.image} size="small" highlight />
               </Link>
             </li>
           )}
--- src/components/ui/Avatar.tsx
+++ src/components/ui/Avatar.tsx
@@ -1,22 +1,37 @@
 ﻿type Props = {
   image?: string | null;
-  size?: 'sm' | 'md';
-  border?: boolean;
+  size?: 'small' | 'normal';
+  highlight?: boolean;
 };
 
-export default function Avatar({ image, size = 'sm', border = true }: Props) {
+export default function Avatar({
+  image,
+  size = 'normal',
+  highlight = false
+}: Props) {
   return (
-    <div
-      className={`rounded-full ${
-        border && 'bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-300'
-      } ${size === 'sm' ? 'w-9 h-9' : 'w-[3.25rem] h-[3.25rem]'}`}
-    >
+    <div className={getContainerStyle(size, highlight)}>
       <img
-        className={`rounded-full ${border && 'p-[0.1rem]'}`}
+        className={`bg-white rounded-full ${getImageSizeStyle(size)}`}
         alt="user profile"
         src={image ?? undefined}
         referrerPolicy="no-referrer"
       />
     </div>
   );
+}
+
+function getContainerStyle(size: string, highlight: boolean): string {
+  const baseStyle = 'rounded-full flex justify-center items-center';
+  const highlightStyle = highlight
+    ? 'bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-300'
+    : '';
+  const sizeStyle = size === 'small' ? 'w-9 h-9' : 'w-[68px] h-[68px]';
+  return `${baseStyle} ${highlightStyle} ${sizeStyle}`;
+}
+
+function getImageSizeStyle(size: string): string {
+  return size === 'small'
+    ? 'w-[34px] h-[34px] p-[0.1rem]'
+    : 'w-16 h-16 p-[0.2rem] ';
 }
