f34e614c9d87a3143c3f6a60fba4f6747b8f06b7
wonieeVicky <hwfongfing@gmail.com>
2023-08-31 21:49:47 +0900

    feat: HomePage 컴포넌트 변경사항 반영

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,28 @@
 // import { getPosts } from '@/service/sanity';
-import UserWidget from '@/components/layout/UserWidget';
+import FollowingBar from '@/components/layout/FollowingBar';
+import PostList from '@/components/layout/PostList';
+import Sidebar from '@/components/layout/Sidebar';
+import { getServerSession } from 'next-auth';
+import { authOptions } from './api/auth/[...nextauth]/route';
+import { redirect } from 'next/navigation';
 
-export default async function Home() {
-  // const posts = await getPosts();
+export default async function HomePage() {
+  const session = await getServerSession(authOptions);
+  const user = session?.user;
+
+  if (!user) {
+    redirect('/auth/signin');
+  }
+
   return (
-    <div className="w-full flex p-5">
-      <div className="flex-1">
-        <div>FollowingBar</div>
-        <div>PostList</div>
+    <section className="w-full flex flex-col md:flex-row max-w-[850px] p-4">
+      <div className="w-full basis-3/4">
+        <FollowingBar />
+        <PostList />
       </div>
-      <UserWidget />
-    </div>
+      <div className="basis-1/4">
+        <Sidebar user={user} />
+      </div>
+    </section>
   );
 }
