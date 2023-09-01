3fe718236257d3a2e14dc7b7d60f67f10ba44245
wonieeVicky <hwfongfing@gmail.com>
2023-09-01 23:01:44 +0900

    feat: SWRContext 생성 후 layout 에 적용

---
+++ src/context/SWRContext.tsx
@@ -0,0 +1,20 @@
+﻿'use client'; //next에서 context를 사용하기 위해서는 csr 렌더링으로 명시해야 함
+// context는 상태를 가지고 있기 때문에 서버컴포넌트는 상태에 접근 할 수 없음
+import { SWRConfig } from 'swr';
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function SWRContext({ children }: Props) {
+  return (
+    <SWRConfig
+      value={{
+        refreshInterval: 3000,
+        fetcher: (resource, init) => fetch(resource, init).then((res) => res.json())
+      }}
+    >
+      {children}
+    </SWRConfig>
+  );
+}
