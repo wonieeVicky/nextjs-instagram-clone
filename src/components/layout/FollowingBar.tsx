105d4b2e1f4c0bf1b5870c4765e36f80044e38b9
wonieeVicky <hwfongfing@gmail.com>
2023-09-03 18:13:12 +0900

    feat: MultiCarousel 유틸 컴포넌트 추가, followingBar 내 getFollowings 추가

--- src/components/layout/FollowingBar.tsx
+++ src/components/layout/FollowingBar.tsx
@@ -1,3 +1,29 @@
-﻿export default function FollowingBar() {
-  return <p>FollowingBar</p>;
+﻿import { User } from '@/model/user';
+import { getFollowings } from '@/service/user';
+import Avatar from '../ui/Avatar';
+import MultiCarousel from '../MultiCarousel';
+
+type Props = {
+  user: User;
+};
+
+export default async function FollowingBar({ user }: Props) {
+  const followings = await getFollowings({ username: user.username });
+
+  if (!followings) {
+    return;
+  }
+
+  return (
+    <div className="border p-5 rounded-md">
+      <MultiCarousel>
+        {followings.map(({ name, username, email, image }: User) => (
+          <div key={username} className="text-center border">
+            <Avatar image={image} highlight />
+            {username}
+          </div>
+        ))}
+      </MultiCarousel>
+    </div>
+  );
 }
