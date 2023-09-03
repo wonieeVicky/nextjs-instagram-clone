6658903882434df236f25d0c86bfc3d8e90c6da0
wonieeVicky <hwfongfing@gmail.com>
2023-09-03 18:13:52 +0900

    chore: FollowingBar에 user 정보 상속

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,8 +16,8 @@ export default async function HomePage() {
 
   return (
     <section className="w-full flex flex-col md:flex-row max-w-[850px] p-4">
-      <div className="w-full basis-3/4">
-        <FollowingBar />
+      <div className="w-full basis-3/4 pr-10">
+        <FollowingBar user={user} />
         <PostList />
       </div>
       <div className="basis-1/4">
