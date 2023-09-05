42fafae9507b824833a71303a8d8fb6303dcf999
wonieeVicky <hwfongfing@gmail.com>
2023-09-05 23:09:21 +0900

    chore: 미사용 코드 및 주석 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import { getServerSession } from 'next-auth';
 import { authOptions } from './api/auth/[...nextauth]/route';
 import { redirect } from 'next/navigation';
 
+// SSR : 사용자의 요청이 올 때마다 서버에서 렌더링. (유저정보로 유의미한 정보를 보여줘야 하므로)
 export default async function HomePage() {
   const session = await getServerSession(authOptions);
   const user = session?.user;
@@ -17,7 +18,7 @@ export default async function HomePage() {
   return (
     <section className="w-full flex flex-col md:flex-row max-w-[850px] p-4">
       <div className="w-full basis-3/4 pr-10">
-        <FollowingBar user={user} />
+        <FollowingBar />
         <PostList />
       </div>
       <div className="basis-1/4">
