5ddee0d1cdc7d08c9572b9a88566908a01df8654
wonieeVicky <hwfongfing@gmail.com>
2023-09-05 23:08:38 +0900

    chore: SWRContext > SWRConfigContext

--- src/context/SWRConfigContext.tsx
+++ src/context/SWRConfigContext.tsx
@@ -6,12 +6,13 @@ type Props = {
   children: React.ReactNode;
 };
 
-export default function SWRContext({ children }: Props) {
+export default function SWRConfigContext({ children }: Props) {
   return (
     <SWRConfig
       value={{
         refreshInterval: 3000,
-        fetcher: (resource, init) => fetch(resource, init).then((res) => res.json())
+        fetcher: (resource, init) =>
+          fetch(resource, init).then((res) => res.json())
       }}
     >
       {children}
