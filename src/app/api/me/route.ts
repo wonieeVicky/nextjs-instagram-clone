56dedf8ff80c100fd41e978cb5f0274d674f9037
wonieeVicky <hwfongfing@gmail.com>
2023-09-05 23:10:17 +0900

    feat: api/me API 구현 - 헤더에 있는 session.user 정보로 getUserByUsername 함수 실행

---
+++ src/app/api/me/route.ts
@@ -0,0 +1,18 @@
+﻿import { getServerSession } from 'next-auth';
+import { NextResponse } from 'next/server';
+import { authOptions } from '../auth/[...nextauth]/route';
+import { getUserByUsername } from '@/service/user';
+
+export async function GET(request: Request) {
+  // token을 분석/해독해서 고객 정보를 가져온다.
+  const session = await getServerSession(authOptions);
+  const user = session?.user;
+
+  if (!user) {
+    return new Response('Unauthorized', { status: 401 }); // 401 Unauthorized
+  }
+
+  return getUserByUsername(user.username).then((data) =>
+    NextResponse.json(data)
+  );
+}
--- src/components/layout/FollowingBar.tsx
+++ src/components/layout/FollowingBar.tsx
@@ -1,29 +1,38 @@
-﻿import { User } from '@/model/user';
-import { getFollowings } from '@/service/user';
-import Avatar from '../ui/Avatar';
-import MultiCarousel from '../MultiCarousel';
+﻿'use client';
+import { User } from '@/model/user';
+
+import useSWR from 'swr';
 
 type Props = {
   user: User;
 };
 
-export default async function FollowingBar({ user }: Props) {
-  const followings = await getFollowings({ username: user.username });
+// 사용자의 유저 정보로 팔로잉 리스트를 받아와야 함 => SSR로 구현 시 과부하. CSR로 구현
+export default function FollowingBar() {
+  // 1. 클라이언트 컴포넌트에서 백엔드에게 api/me 사용자의 정보를 얻어온다.
+  const { data, isLoading } = useSWR('/api/me');
+  console.log('1:', data);
 
-  if (!followings) {
-    return;
-  }
+  // 2. 백엔드에서는 현재 로그인된 사용자의 세션 정보를 이용해서 Sanity에 접근한다. (getUserByUsername)
+  // 3. 백엔드에서 사용자의 상세 정보(followings)를 Sanity에서 가지고 온다.
+  // 4. 여기에서 클라이언트 컴포넌트에서 followings의 정보를 UI에 보여준다(image, username)
+
+  // const followings = await getFollowings({ username: user.username });
+
+  // if (!followings) {
+  //   return;
+  // }
 
   return (
-    <div className="border p-5 rounded-md">
-      <MultiCarousel>
+    <div className="border p-8 rounded-md">
+      {/* <MultiCarousel>
         {followings.map(({ name, username, email, image }: User) => (
-          <div key={username} className="text-center border">
+          <div key={username} className="flex flex-col items-center">
             <Avatar image={image} highlight />
-            {username}
+            <p>{username}</p>
           </div>
         ))}
-      </MultiCarousel>
+      </MultiCarousel> */}
     </div>
   );
 }
