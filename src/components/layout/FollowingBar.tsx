27a3c8dbc89f85fb7017b73c71b0796cdd9aabeb
wonieeVicky <hwfongfing@gmail.com>
2023-09-07 23:31:36 +0900

    feat: followingBar style

--- src/components/layout/FollowingBar.tsx
+++ src/components/layout/FollowingBar.tsx
@@ -17,19 +17,24 @@ export default function FollowingBar() {
   const users = data?.following;
 
   return (
-    <section>
+    <section className="w-full flex justify-center items-center p-4 shadow-sm shadow-neutral-300 mb-4 rounded-lg min-h-[90px] overflow-x-auto">
       {loading ? (
         <PropagateLoader size={8} color="green" />
       ) : (
         (!users || users.length === 0) && <p>{`You don't have following`}</p>
       )}
       {users && users.length > 0 && (
-        <ul>
-          {users.map(({ image, username }) => (
-            <li key={username}>
-              <Link href={`/user/${username}`}>
+        <ul className="w-full flex gap-2">
+          {users.map(({ image, username }, i) => (
+            <li key={i}>
+              <Link
+                className="flex flex-col items-center w-20"
+                href={`/user/${username}`}
+              >
                 <Avatar image={image} highlight />
-                <p>{username}</p>
+                <p className="w-full text-sm text-center text-ellipsis overflow-hidden">
+                  {username}
+                </p>
               </Link>
             </li>
           ))}
