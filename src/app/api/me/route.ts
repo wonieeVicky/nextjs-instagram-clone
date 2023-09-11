7f430ae73b08fa2541c2476226502777e159a835
wonieeVicky <hwfongfing@gmail.com>
2023-09-11 22:55:54 +0900

    chore: 미사용 매개변수 삭제

--- src/app/api/me/route.ts
+++ src/app/api/me/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 import { authOptions } from '../auth/[...nextauth]/route';
 import { getUserByUsername } from '@/service/user';
 
-export async function GET(request: Request) {
+export async function GET() {
   // token을 분석/해독해서 고객 정보를 가져온다.
   const session = await getServerSession(authOptions);
   const user = session?.user;
