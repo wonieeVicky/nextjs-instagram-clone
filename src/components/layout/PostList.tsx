9dd8e55403989f9fdc3591e1c3101e16369ebb97
wonieeVicky <hwfongfing@gmail.com>
2023-09-11 22:57:42 +0900

    feat: post 타입 생성 및 PostList 내 swr event 및 typing 적용

--- src/components/layout/PostList.tsx
+++ src/components/layout/PostList.tsx
@@ -1,3 +1,13 @@
-﻿export default function PostList() {
-  return <p>PostList</p>;
+﻿'use client';
+
+import { SimplePost } from '@/model/post';
+import useSWR from 'swr';
+
+export default function PostList() {
+  const { data: posts, isLoading: loading } =
+    useSWR<SimplePost[]>('/api/posts');
+
+  return (
+    <ul>{posts && posts.map((post) => <li key={post.id}>{post.text}</li>)}</ul>
+  );
 }
---
+++ src/model/post.ts
@@ -0,0 +1,20 @@
+﻿export type Comment = {
+  comment: string;
+  username: string;
+  image: string;
+};
+
+export type SimplePost = Omit<FullPost, 'comments'> & {
+  comments: number;
+};
+
+export type FullPost = {
+  id: string;
+  username: string;
+  userImage: string;
+  iamge: string;
+  text: string;
+  createdAt: string;
+  likes: string[];
+  comments: Comment[];
+};
