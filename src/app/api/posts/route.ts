a7bc5d70ee004f5f7e57946b45e07b56c0f471eb
wonieeVicky <hwfongfing@gmail.com>
2023-09-11 22:57:02 +0900

    feat: GET api/posts 구현

---
+++ src/app/api/posts/route.ts
@@ -0,0 +1,18 @@
+﻿import { getServerSession } from 'next-auth';
+import { NextResponse } from 'next/server';
+import { authOptions } from '../auth/[...nextauth]/route';
+import { getFollowingPostsOf } from '@/service/posts';
+
+export async function GET() {
+  // token을 분석/해독해서 고객 정보를 가져온다.
+  const session = await getServerSession(authOptions);
+  const user = session?.user;
+
+  if (!user) {
+    return new Response('Unauthorized', { status: 401 }); // 401 Unauthorized
+  }
+
+  return getFollowingPostsOf(user.username).then((data) =>
+    NextResponse.json(data)
+  );
+}
