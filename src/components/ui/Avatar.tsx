15b6eed1937aab597d9f0519526c290ff219eeb4
wonieeVicky <hwfongfing@gmail.com>
2023-09-12 23:13:38 +0900

    feat: Avatar 내 medium 사이즈 추가

--- src/components/ui/Avatar.tsx
+++ src/components/ui/Avatar.tsx
@@ -1,6 +1,6 @@
 ﻿type Props = {
   image?: string | null;
-  size?: 'small' | 'normal';
+  size?: 'small' | 'normal' | 'medium';
   highlight?: boolean;
 };
 
@@ -28,12 +28,20 @@ function getContainerStyle(size: string, highlight: boolean): string {
   const highlightStyle = highlight
     ? 'bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-300'
     : '';
-  const sizeStyle = size === 'small' ? 'w-9 h-9' : 'w-[68px] h-[68px]';
+  const mapSizeToStyle: Record<string, string> = {
+    small: 'w-9 h-9',
+    normal: 'w-[68px] h-[68px]',
+    medium: 'w-[45px] h-[45px]'
+  };
+  const sizeStyle = mapSizeToStyle[size];
   return `${baseStyle} ${highlightStyle} ${sizeStyle}`;
 }
 
-function getImageSizeStyle(size: string): string {
-  return size === 'small'
-    ? 'w-[34px] h-[34px] p-[0.1rem]'
-    : 'w-16 h-16 p-[0.2rem] ';
+function getImageSizeStyle(size: 'small' | 'normal' | 'medium'): string {
+  const mapSizeToStyle: Record<'small' | 'normal' | 'medium', string> = {
+    small: 'w-[34px] h-[34px] p-[0.1rem]',
+    normal: 'w-16 h-16 p-[0.2rem]',
+    medium: 'p-[0.1rem] w-[41px] h-[41px]'
+  };
+  return mapSizeToStyle[size];
 }
