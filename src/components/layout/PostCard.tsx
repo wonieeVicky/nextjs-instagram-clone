35f8eb5a032bb23e65abbd4ef46803fd77ebb1d3
wonieeVicky <hwfongfing@gmail.com>
2023-09-16 21:28:26 +0900

    feat:PostList 컴포넌트 스타일 구현, PostCard에 적용

--- src/components/layout/PostCard.tsx
+++ src/components/layout/PostCard.tsx
@@ -17,51 +17,92 @@ export default function PostCard({ post, user }: Props) {
   const { userImage, username, image, likes, text, createdAt } = post;
 
   return (
-    <div className="w-full flex shadow-sm shadow-neutral-300 mb-4 rounded-lg flex-col">
-      <div className="p-2 flex items-center">
-        <Avatar image={userImage} size="medium" highlight />
-        &nbsp;&nbsp;
-        <span className="font-bold">{username}</span>
+    <article className="rounded-lg shadow-md border border-gray-200">
+      <div className="flex items-center p-2">
+        <Avatar image={userImage} highlight size="medium" />
+        <span className="text-gray-900 font-bold ml-2">{username}</span>
       </div>
-      <div>
-        <img src={image} alt="" />
+      <Image
+        src={image}
+        className="w-full object-cover aspect-square"
+        alt={`photo by ${username}`}
+        width={500}
+        height={500}
+      />
+      <div className="flex justify-between my-2 px-4">
+        <HeartIcon />
+        <BookmarkIcon />
       </div>
-      <div className="py-2">
-        <div className="p-1 px-3 flex justify-between">
-          {user && likes?.includes(user.username) ? (
-            <HeartFillIcon />
-          ) : (
-            <HeartIcon />
-          )}
-          <BookmarkIcon />
-        </div>
-        <div className="p-1 px-3 text-sm font-semibold">
-          {likes?.length || 0} like
-          {likes?.length > 1 ? 's' : ''}
-        </div>
-        <div className="p-1 px-3 text-sm">
-          <span className="font-bold">{username}</span>
-          &nbsp;&nbsp;
-          <span>{text}</span>
-        </div>
-        <div className="p-1 px-3">
-          <span className="text-xs font-light text-gray-500">
-            {parseDate(createdAt)?.toUpperCase()}
-          </span>
-        </div>
-      </div>
-      <div className="p-2 flex justify-between items-center border-t border-gray-300">
-        <div className="flex items-center">
+      <div className="px-4 py-1">
+        <p className="text-sm font-bold mb-2">{`${likes?.length ?? 0} ${
+          likes?.length > 1 ? 'likes' : 'like'
+        }`}</p>
+        <p>
+          <span className="font-bold mr-1">{username}</span>
+          {text}
+        </p>
+        <p className="text-xs text-neutral-500 uppercase my-2">
+          {parseDate(createdAt)}
+        </p>
+        <form className="flex items-center border-t border-neutral-300">
           <SmileIcon />
-          &nbsp;&nbsp;
           <input
             type="text"
             placeholder="Add a comment..."
-            className="h-9 w-96 bg-neutral-50 text-gray-500 border-0 text-sm placeholder-gray-400 focus:outline-none focus:ring-white"
+            className="w-full ml-2 border-none outline-none p-3"
           />
-        </div>
-        <button className="text-cyan-600 font-semibold text-sm">Post</button>
+          <button className="font-bold text-sky-500 ml-2">Post</button>
+        </form>
       </div>
-    </div>
+    </article>
   );
+
+  // return (
+  //   <div className="w-full flex shadow-sm shadow-neutral-300 mb-4 rounded-lg flex-col">
+  //     <div className="p-2 flex items-center">
+  //       <Avatar image={userImage} size="medium" highlight />
+  //       &nbsp;&nbsp;
+  //       <span className="font-bold">{username}</span>
+  //     </div>
+  //     <div>
+  //       <img src={image} alt="" />
+  //     </div>
+  //     <div className="py-2">
+  //       <div className="p-1 px-3 flex justify-between">
+  //         {user && likes?.includes(user.username) ? (
+  //           <HeartFillIcon />
+  //         ) : (
+  //           <HeartIcon />
+  //         )}
+  //         <BookmarkIcon />
+  //       </div>
+  //       <div className="p-1 px-3 text-sm font-semibold">
+  //         {likes?.length || 0} like
+  //         {likes?.length > 1 ? 's' : ''}
+  //       </div>
+  //       <div className="p-1 px-3 text-sm">
+  //         <span className="font-bold">{username}</span>
+  //         &nbsp;&nbsp;
+  //         <span>{text}</span>
+  //       </div>
+  //       <div className="p-1 px-3">
+  //         <span className="text-xs font-light text-gray-500">
+  //           {parseDate(createdAt)?.toUpperCase()}
+  //         </span>
+  //       </div>
+  //     </div>
+  //     <div className="p-2 flex justify-between items-center border-t border-gray-300">
+  //       <div className="flex items-center">
+  //         <SmileIcon />
+  //         &nbsp;&nbsp;
+  //         <input
+  //           type="text"
+  //           placeholder="Add a comment..."
+  //           className="h-9 w-96 bg-neutral-50 text-gray-500 border-0 text-sm placeholder-gray-400 focus:outline-none focus:ring-white"
+  //         />
+  //       </div>
+  //       <button className="text-cyan-600 font-semibold text-sm">Post</button>
+  //     </div>
+  //   </div>
+  // );
 }
--- src/components/layout/PostList.tsx
+++ src/components/layout/PostList.tsx
@@ -3,7 +3,7 @@
 import { SimplePost } from '@/model/post';
 import useSWR from 'swr';
 import { useSession } from 'next-auth/react';
-import { PropagateLoader } from 'react-spinners';
+import { GridLoader, PropagateLoader } from 'react-spinners';
 import PostCard from './PostCard';
 
 export default function PostList() {
@@ -12,28 +12,49 @@ export default function PostList() {
     useSWR<SimplePost[]>('/api/posts');
   const user = session?.user;
 
+  console.log(posts);
+
   return (
-    <section className="w-full flex justify-center items-center">
-      {loading ? (
-        <div className="p-2">
-          <PropagateLoader size={8} color="green" />
+    <section>
+      {loading && (
+        <div className="text-center mt-32">
+          <GridLoader color="green" />
         </div>
-      ) : (
-        posts!.length === 0 && <p>{`You don't have posts`}</p>
       )}
-      <ul>
-        {posts && user ? (
-          posts.map((post) => (
-            <li key={post.id}>
+      {posts && user && (
+        <ul>
+          {posts.map((post) => (
+            <li key={post.id} className="mb-6">
               <PostCard post={post} user={user} />
             </li>
-          ))
-        ) : loading ? (
-          <PropagateLoader size={8} color="green" />
-        ) : (
-          posts!.length === 0 && <p>{`You don't have posts`}</p>
-        )}
-      </ul>
+          ))}
+        </ul>
+      )}
     </section>
   );
+
+  // return (
+  //   <section className="w-full flex justify-center items-center">
+  //     {loading ? (
+  //       <div className="p-2">
+  //         <PropagateLoader size={8} color="green" />
+  //       </div>
+  //     ) : (
+  //       posts!.length === 0 && <p>{`You don't have posts`}</p>
+  //     )}
+  //     <ul>
+  //       {posts && user ? (
+  //         posts.map((post) => (
+  //           <li key={post.id}>
+  //             <PostCard post={post} user={user} />
+  //           </li>
+  //         ))
+  //       ) : loading ? (
+  //         <PropagateLoader size={8} color="green" />
+  //       ) : (
+  //         posts!.length === 0 && <p>{`You don't have posts`}</p>
+  //       )}
+  //     </ul>
+  //   </section>
+  // );
 }
