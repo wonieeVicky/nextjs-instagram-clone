5741ea2b0a68058d935b5684aab6775c035cac96
wonieeVicky <hwfongfing@gmail.com>
2023-09-18 23:29:53 +0900

    feat: ModalContent 내 이미지 노출 구현, CloseIcon 컴포넌트 추가 및 적용

---
+++ src/components/ui/ModalContent.tsx
@@ -0,0 +1,39 @@
+﻿import { SimplePost } from '@/model/post';
+import { AiOutlineCloseCircle } from 'react-icons/ai';
+import CloseIcon from './icons/CloseIcon';
+import Image from 'next/image';
+
+type Props = {
+  onClose: () => void;
+  post: SimplePost;
+};
+
+export default function ModalContent({ onClose, post }: Props) {
+  const { userImage, username, image, likes, text, createdAt } = post;
+
+  return (
+    <>
+      <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+        <div className="relative w-auto my-6 mx-auto max-w-5xl">
+          <div className="border-0 shadow-lg relative flex w-auto h-auto bg-white outline-none focus:outline-none">
+            <Image
+              src={image}
+              className="w-[1000px] object-cover aspect-square cursor-pointer"
+              alt={`photo by ${username}`}
+              width={500}
+              height={500}
+            />
+            <div className="bg-white min-w-[400px]"></div>
+          </div>
+        </div>
+      </div>
+      <div className="fixed inset-0 z-40 bg-black opacity-70" />
+      <div
+        onClick={onClose}
+        className="cursor-pointer fixed right-5 top-5 z-50"
+      >
+        <CloseIcon color="white" />
+      </div>
+    </>
+  );
+}
---
+++ src/components/ui/icons/CloseIcon.tsx
@@ -0,0 +1,5 @@
+﻿import { IoClose } from 'react-icons/io5';
+
+export default function CloseIcon({ color }: { color?: string }) {
+  return <IoClose className="w-7 h-7" color={color} />;
+}
