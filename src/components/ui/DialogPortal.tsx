b9a8b69a85e1331f92997e25fe9381974930d0c8
wonieeVicky <hwfongfing@gmail.com>
2023-09-18 23:31:47 +0900

    feat: DialogPortal 컴포넌트 구현. 모달 on/off 액션 구현

---
+++ src/components/ui/DialogPortal.tsx
@@ -0,0 +1,26 @@
+﻿import { SimplePost } from '@/model/post';
+import { Dispatch, SetStateAction } from 'react';
+import { createPortal } from 'react-dom';
+import ModalContent from './ModalContent';
+
+type Props = {
+  showDialog: boolean;
+  setShowDialog: Dispatch<SetStateAction<boolean>>;
+  post: SimplePost;
+};
+
+export default function DialogPortal({
+  showDialog,
+  setShowDialog,
+  post
+}: Props) {
+  return (
+    <>
+      {showDialog &&
+        createPortal(
+          <ModalContent onClose={() => setShowDialog(false)} post={post} />,
+          document.getElementById('dialog-root')!
+        )}
+    </>
+  );
+}
