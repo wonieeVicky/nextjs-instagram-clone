28d9d3b082c24751dbb96b75b543b845f9e198bb
wonieeVicky <hwfongfing@gmail.com>
2023-09-20 23:58:51 +0900

    feat: PostModal 기본 포맷 구현

---
+++ src/components/ui/PostModal.tsx
@@ -0,0 +1,24 @@
+﻿import CloseIcon from './icons/CloseIcon';
+
+type Props = {
+  children: React.ReactNode;
+  onClose: () => void;
+};
+export default function PostModal({ children, onClose }: Props) {
+  return (
+    <section
+      className="fixed top-0 left-0 flex flex-col justify-center items-center w-full h-full z-50 bg-neutral-900/70"
+      onClick={(event) => {
+        if (event.target === event.currentTarget) {
+          // 외부 섹션 태그를 다시 클릭했을 경우 모달을 닫는다.
+          onClose();
+        }
+      }}
+    >
+      <button onClick={onClose} className="fixed top-0 right-0 p-8 text-white">
+        <CloseIcon />
+      </button>
+      {children}
+    </section>
+  );
+}
