f49627cac55452d94f9982d5af339981d76461c9
wonieeVicky <hwfongfing@gmail.com>
2023-09-25 00:01:25 +0900

    feat: SearchPage 기본 인풋/리스트 레이아웃 구현, userList 모든 유저 노출 처리

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,3 +1,28 @@
-﻿export default function SearchPage() {
-  return <>Search Page</>;
+﻿'use client';
+
+import GridSpinner from '@/components/ui/GridSpinner';
+import UserList from '@/components/ui/UserList';
+import { DetailUser } from '@/model/user';
+import useSWR from 'swr';
+
+export default function SearchPage() {
+  const { data, isLoading: loading } = useSWR<DetailUser[]>('/api/users');
+
+  return (
+    <section className="w-full max-w-screen-xl mx-auto">
+      <div className="block p-5 max-w-screen-md mx-auto">
+        <input
+          type="text"
+          placeholder="Search for a username or name"
+          className="w-full h-12 placeholder:text-neutral-400 placeholder:text-lg border-neutral-400 focus:border-none"
+        />
+      </div>
+      {loading && (
+        <div className="text-center mt-32">
+          <GridSpinner />
+        </div>
+      )}
+      {data && <UserList list={data} />}
+    </section>
+  );
 }
---
+++ src/components/ui/UserList.tsx
@@ -0,0 +1,32 @@
+﻿import { DetailUser } from '@/model/user';
+import Avatar from './Avatar';
+import Link from 'next/link';
+
+type Props = {
+  list: DetailUser[];
+};
+export default function UserList({ list }: Props) {
+  return (
+    <section>
+      {list.map(({ name, username, image, following, followers }) => {
+        return (
+          <Link href={`/user/${username}`} key={username}>
+            <div className="flex p-3 max-w-2xl m-auto mb-2 items-center border border-neutral-200">
+              <div className="mr-3">
+                <Avatar image={image} size="large" />
+              </div>
+              <div className="flex-auto">
+                <h2 className="text-sm font-bold">{username}</h2>
+                <p className="text-sm text-neutral-500">{name}</p>
+                <p className="text-sm text-neutral-500">
+                  {followers?.length || 0} followers {following?.length || 0}{' '}
+                  following
+                </p>
+              </div>
+            </div>
+          </Link>
+        );
+      })}
+    </section>
+  );
+}
