c4865d4e19646e3e14ea525c7b7a6e0f772be955
wonieeVicky <hwfongfing@gmail.com>
2023-09-26 23:48:58 +0900

    feat: SearchPage 내 csr 페이지 컴포넌트로 분리

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,43 +1,9 @@
-﻿'use client';
-
-import GridSpinner from '@/components/ui/GridSpinner';
-import UserList from '@/components/ui/UserList';
-import { DetailUser } from '@/model/user';
-import { useState } from 'react';
-import useSWR from 'swr';
+﻿import UserSearch from '@/components/ui/UserSearch';
 
 export default function SearchPage() {
-  const [value, setValue] = useState<undefined | string>(undefined);
-  const [type, setType] = useState<'all' | 'search'>('all');
-
-  // fetch UserList from /api/users API with params
-  const { data, isLoading: loading } = useSWR<DetailUser[]>(
-    `/api/users?type=${type}&value=${value}`
-  );
-
   return (
     <section className="w-full max-w-screen-xl mx-auto">
-      <div className="block p-5 max-w-screen-md mx-auto">
-        <input
-          type="text"
-          placeholder="Search for a username or name"
-          className="w-full h-12 placeholder:text-neutral-400 placeholder:text-lg border-neutral-400 focus:border-none focus:ring-neutral-500"
-          onKeyUp={(e) => {
-            if (!e.currentTarget.value.trim()) {
-              setType('all');
-              return;
-            }
-            setValue(e.currentTarget.value);
-            setType('search');
-          }}
-        />
-      </div>
-      {loading && (
-        <div className="text-center mt-32">
-          <GridSpinner />
-        </div>
-      )}
-      {data && <UserList list={data} />}
+      <UserSearch />
     </section>
   );
 }
