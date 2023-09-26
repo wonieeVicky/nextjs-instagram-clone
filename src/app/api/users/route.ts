dd6316b13879e8df0b7952313c2d0d599c6db7cc
wonieeVicky <hwfongfing@gmail.com>
2023-09-26 23:24:05 +0900

    feat: users GET 내 request에 전달된 searchParams를 기반으로 getUsers service 함수 실행

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,9 +1,9 @@
 ﻿import { getServerSession } from 'next-auth';
 import { NextResponse } from 'next/server';
 import { authOptions } from '../auth/[...nextauth]/route';
-import { getAllUsers } from '@/service/user';
+import { getUsers } from '@/service/user';
 
-export async function GET() {
+export async function GET(request: Request) {
   // token을 분석/해독해서 고객 정보를 가져온다.
   const session = await getServerSession(authOptions);
   const user = session?.user;
@@ -12,5 +12,10 @@ export async function GET() {
     return new Response('Unauthorized', { status: 401 }); // 401 Unauthorized
   }
 
-  return getAllUsers().then((data) => NextResponse.json(data));
+  // get searchParams
+  const { searchParams } = new URL(request.url);
+  const type = searchParams.get('type') as 'all' | 'search';
+  const data = searchParams.get('value') || undefined;
+
+  return getUsers(type, data).then((data) => NextResponse.json(data));
 }
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -3,10 +3,17 @@
 import GridSpinner from '@/components/ui/GridSpinner';
 import UserList from '@/components/ui/UserList';
 import { DetailUser } from '@/model/user';
+import { useState } from 'react';
 import useSWR from 'swr';
 
 export default function SearchPage() {
-  const { data, isLoading: loading } = useSWR<DetailUser[]>('/api/users');
+  const [value, setValue] = useState<undefined | string>(undefined);
+  const [type, setType] = useState<'all' | 'search'>('all');
+
+  // fetch UserList from /api/users API with params
+  const { data, isLoading: loading } = useSWR<DetailUser[]>(
+    `/api/users?type=${type}&value=${value}`
+  );
 
   return (
     <section className="w-full max-w-screen-xl mx-auto">
@@ -15,6 +22,14 @@ export default function SearchPage() {
           type="text"
           placeholder="Search for a username or name"
           className="w-full h-12 placeholder:text-neutral-400 placeholder:text-lg border-neutral-400 focus:border-none focus:ring-neutral-500"
+          onKeyUp={(e) => {
+            if (!e.currentTarget.value.trim()) {
+              setType('all');
+              return;
+            }
+            setValue(e.currentTarget.value);
+            setType('search');
+          }}
         />
       </div>
       {loading && (
