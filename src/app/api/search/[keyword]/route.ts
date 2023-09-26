b0a397fe52653053269b9b5a09a70f295a0d5759
wonieeVicky <hwfongfing@gmail.com>
2023-09-26 23:48:35 +0900

    refactor: search용 API 추가, context.params.keyword가 존재/미존재 케이스 route 분리

---
+++ src/app/api/search/[keyword]/route.ts
@@ -0,0 +1,12 @@
+﻿import { searchUsers } from '@/service/user';
+import { NextRequest, NextResponse } from 'next/server';
+
+type Context = {
+  params: { keyword: string };
+};
+export async function GET(_: NextRequest, context: Context) {
+  // 로그인 여부를 보지 않는다.
+
+  return searchUsers(context.params.keyword) //
+    .then((data) => NextResponse.json(data));
+}
---
+++ src/app/api/search/route.ts
@@ -0,0 +1,7 @@
+﻿import { searchUsers } from '@/service/user';
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  // 로그인 여부를 보지 않는다.
+  return searchUsers().then((data) => NextResponse.json(data));
+}
