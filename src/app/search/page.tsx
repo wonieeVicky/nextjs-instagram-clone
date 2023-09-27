e1d2de4937040aa3e014c8bfe428448a5f40238f
wonieeVicky <hwfongfing@gmail.com>
2023-09-27 23:26:21 +0900

    feat: UserSearch 내 UserCard 적용, 페이지 스타일 최적화

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -2,8 +2,8 @@
 
 export default function SearchPage() {
   return (
-    <section className="w-full max-w-screen-xl mx-auto">
+    <>
       <UserSearch />
-    </section>
+    </>
   );
 }
--- src/components/ui/UserSearch.tsx
+++ src/components/ui/UserSearch.tsx
@@ -4,6 +4,7 @@ import { ProfileUser } from '@/model/user';
 import { FormEvent, useState } from 'react';
 import useSWR from 'swr';
 import GridSpinner from './GridSpinner';
+import UserCard from './UserCard';
 
 export default function UserSearch() {
   const [keyword, setKeyword] = useState('');
@@ -18,28 +19,28 @@ export default function UserSearch() {
   };
 
   return (
-    <>
-      <form onSubmit={onSubmit}>
+    <section className="w-full max-w-2xl my-4 flex flex-col items-center">
+      <form className="w-full mb-4" onSubmit={onSubmit}>
         <input
           type="text"
-          autoFocus
           placeholder="Search for a username or name"
+          className="w-full h-12 placeholder:text-neutral-400 placeholder:text-lg border-neutral-400 focus:border-none focus:ring-neutral-500"
           value={keyword}
+          autoFocus
           onChange={(e) => setKeyword(e.target.value)}
         />
-        <button type="submit">Search</button>
       </form>
       {error && <p>error</p>}
       {isLoading && <GridSpinner />}
       {!isLoading && !error && users?.length === 0 && <p>No results</p>}
-      <ul>
+      <ul className="w-full p-4">
         {users &&
           users.map((user) => (
             <li key={user.username}>
-              <p>{user.username}</p>
+              <UserCard user={user} />
             </li>
           ))}
       </ul>
-    </>
+    </section>
   );
 }
