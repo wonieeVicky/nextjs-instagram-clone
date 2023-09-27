1458404aa5562a1cec16e4c3fed43905a1ebebae
wonieeVicky <hwfongfing@gmail.com>
2023-09-27 23:11:53 +0900

    feat: ProfileUser type 추가, UserSearch 내 렌더링 엘리먼트 정의

--- src/components/ui/UserSearch.tsx
+++ src/components/ui/UserSearch.tsx
@@ -1,15 +1,45 @@
 ﻿'use client';
 
-import { useState } from 'react';
+import { ProfileUser } from '@/model/user';
+import { FormEvent, useState } from 'react';
 import useSWR from 'swr';
+import GridSpinner from './GridSpinner';
 
 export default function UserSearch() {
-  // /api/search/${keyword}
-  // 검색하는 keyword가 있다면 /api/search/bob -> 유저네임이나, 네임
-  // 검색하는 keyword가 없다면 /api/search -> 전제 유저
   const [keyword, setKeyword] = useState('');
-  const { data, isLoading, error } = useSWR(`/api/search/${keyword}`);
-  console.log(data);
+  const {
+    data: users,
+    isLoading,
+    error
+  } = useSWR<ProfileUser[]>(`/api/search/${keyword}`);
 
-  return <></>;
+  const onSubmit = (e: FormEvent) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          autoFocus
+          placeholder="Search for a username or name"
+          value={keyword}
+          onChange={(e) => setKeyword(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+      {error && <p>error</p>}
+      {isLoading && <GridSpinner />}
+      {!isLoading && !error && users?.length === 0 && <p>No results</p>}
+      <ul>
+        {users &&
+          users.map((user) => (
+            <li key={user.username}>
+              <p>{user.username}</p>
+            </li>
+          ))}
+      </ul>
+    </>
+  );
 }
