dc6952885eba115d008102ca569ee328f5ff7dda
wonieeVicky <hwfongfing@gmail.com>
2023-10-02 23:13:17 +0900

    fix: user/[id]/page -> uesr/[username]/page

--- src/app/user/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-﻿import UserInfo from '@/components/ui/UserInfo';
-
-export default async function UserPage() {
-  return (
-    <section className="w-full">
-      <UserInfo />
-    </section>
-  );
-}
---
+++ src/app/user/[username]/page.tsx
@@ -0,0 +1,22 @@
+﻿import UserProfile from '@/components/ui/UserProfile';
+
+import { getUserForProfile } from '@/service/user';
+import { notFound } from 'next/navigation';
+
+type Props = {
+  params: {
+    username: string;
+  };
+};
+
+export default async function UserPage({ params: { username } }: Props) {
+  // 하단: 3개의 탭(posts, liked, bookmarks)
+  // 상단: 사용자의 프로필 이미지와 정보(username, name, 숫자)
+  const user = await getUserForProfile(username);
+
+  if (!user) {
+    notFound();
+  }
+
+  return <UserProfile user={user} />;
+}
