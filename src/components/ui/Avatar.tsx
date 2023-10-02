55371303bfb7c1d2d7476ab5ed4ca44fee1dd016
wonieeVicky <hwfongfing@gmail.com>
2023-10-02 23:44:23 +0900

    refactor: Avatar size mapping

--- src/components/ui/Avatar.tsx
+++ src/components/ui/Avatar.tsx
@@ -13,9 +13,9 @@ export default function Avatar({
   return (
     <div className={getContainerStyle(size, highlight)}>
       <img
-        className={`bg-white object-cover rounded-full ${getImageSizeStyle(
-          size
-        )}`}
+        className={`bg-white object-cover rounded-full ${
+          getImageSizeStyle(size).image
+        }`}
         alt="user profile"
         src={image ?? undefined}
         referrerPolicy="no-referrer"
@@ -24,27 +24,40 @@ export default function Avatar({
   );
 }
 
+function getImageSizeStyle(size: AvatarSize): ImageSizeStyle {
+  const mapSizeToStyle: Record<AvatarSize, ImageSizeStyle> = {
+    small: {
+      container: 'w-9 h-9',
+      image: 'w-[34px] h-[34px] p-[0.1rem]'
+    },
+    medium: {
+      container: 'w-11 h-11',
+      image: 'w-[42px] h-[42px] p-[0.1rem]'
+    },
+    large: {
+      container: 'w-[68px] h-[68px]',
+      image: 'w-16 h-16 p-[0.2rem]'
+    },
+    xlarge: {
+      container: 'w-[142px] h-[142px]',
+      image: 'w-[138px] h-[138px] p-[0.3rem]'
+    }
+  };
+
+  return mapSizeToStyle[size];
+}
+
 function getContainerStyle(size: AvatarSize, highlight: boolean): string {
   const baseStyle = 'rounded-full flex justify-center items-center';
   const highlightStyle = highlight
     ? 'bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-300'
     : '';
-  const mapSizeToStyle: Record<AvatarSize, string> = {
-    small: 'w-9 h-9',
-    medium: 'w-11 h-11',
-    large: 'w-[68px] h-[68px]',
-    xlarge: 'w-[132px] h-[132px]'
-  };
-  const sizeStyle = mapSizeToStyle[size];
-  return `${baseStyle} ${highlightStyle} ${sizeStyle}`;
+  const { container } = getImageSizeStyle(size);
+
+  return `${baseStyle} ${highlightStyle} ${container}`;
 }
 
-function getImageSizeStyle(size: AvatarSize): string {
-  const mapSizeToStyle: Record<AvatarSize, string> = {
-    small: 'w-[34px] h-[34px] p-[0.1rem]',
-    medium: 'w-[42px] h-[42px] p-[0.1rem]',
-    large: 'w-16 h-16 p-[0.2rem]',
-    xlarge: 'w-32 h-32 p-[0.2rem]'
-  };
-  return mapSizeToStyle[size];
-}
+type ImageSizeStyle = {
+  container: string;
+  image: string;
+};
