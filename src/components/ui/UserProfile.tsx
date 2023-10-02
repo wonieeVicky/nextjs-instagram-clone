1514f1361cdbc66eec3b09b32418b4a066191f17
wonieeVicky <hwfongfing@gmail.com>
2023-10-02 23:27:46 +0900

    feat: UserProfile 내 필요한 정보 호출, FollowButton 구현 및 추가

---
+++ src/components/ui/UserProfile.tsx
@@ -0,0 +1,43 @@
+﻿import { ProfileUser } from '@/model/user';
+import Avatar from './Avatar';
+import FollowButton from './FollowButton';
+
+type Props = {
+  user: ProfileUser;
+};
+
+export default function UserProfile({ user }: Props) {
+  const { image, username, name, following, followers, posts } = user;
+  const info = [
+    {
+      title: 'posts',
+      data: posts
+    },
+    {
+      title: 'followers',
+      data: followers
+    },
+    {
+      title: 'following',
+      data: following
+    }
+  ];
+  return (
+    <section>
+      <Avatar image={image} highlight />
+      <div>
+        <h1>{username}</h1>
+        <FollowButton user={user} />
+        <ul>
+          {info.map(({ title, data }, index) => (
+            <li key={index}>
+              <span>{data}</span>
+              {title}
+            </li>
+          ))}
+        </ul>
+        <p>{name}</p>
+      </div>
+    </section>
+  );
+}
