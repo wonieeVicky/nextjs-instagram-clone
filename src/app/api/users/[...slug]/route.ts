48500dfffeeffc503a1ed2d2739da4d9b477bda6
wonieeVicky <hwfongfing@gmail.com>
2023-10-03 23:36:25 +0900

    feat: /api/users/{username}/{tab} API 구현, 미사용 route 삭제

---
+++ src/app/api/users/[...slug]/route.ts
@@ -0,0 +1,28 @@
+﻿import { getLikedPostsOf, getPostsOf, getSavedPostsOf } from '@/service/posts';
+import { NextRequest, NextResponse } from 'next/server';
+
+type Context = {
+  params: {
+    slug: string[]; // slug/slug/slug...
+  };
+};
+
+export async function GET(_: NextRequest, context: Context) {
+  const { slug } = context.params;
+  if (!slug || !Array.isArray(slug) || slug.length === 0) {
+    return new NextResponse('Bad Request', { status: 400 });
+  }
+
+  const [username, query] = slug;
+
+  const mapServiceFuncToQuery = {
+    posts: getPostsOf,
+    liked: getLikedPostsOf,
+    saved: getSavedPostsOf
+  };
+
+  let request =
+    mapServiceFuncToQuery[(query as 'posts' | 'liked' | 'saved') ?? 'posts'];
+
+  return request(username).then((data) => NextResponse.json(data));
+}
