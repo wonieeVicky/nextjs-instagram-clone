f059cbcf4e3e9a46f188506717bd46d373691f6b
wonieeVicky <hwfongfing@gmail.com>
2023-10-06 22:23:27 +0900

    feat: user 페이지 내 generateMetadata를 사용해 metaData 동적 할당, 내부 공통 fetch func을 react.cache로 처리

--- src/app/user/[username]/page.tsx
+++ src/app/user/[username]/page.tsx
@@ -2,7 +2,9 @@
 import UserProfile from '@/components/ui/UserProfile';
 
 import { getUserForProfile } from '@/service/user';
+import { Metadata } from 'next';
 import { notFound } from 'next/navigation';
+import { cache } from 'react';
 
 type Props = {
   params: {
@@ -10,10 +12,13 @@ type Props = {
   };
 };
 
+// cache함수 getUser를 만들어서 getUserForProfile을 실행시킨다.
+const getUser = cache(async (username: string) => getUserForProfile(username));
+
 export default async function UserPage({ params: { username } }: Props) {
   // 하단: 3개의 탭(posts, liked, bookmarks)
   // 상단: 사용자의 프로필 이미지와 정보(username, name, 숫자)
-  const user = await getUserForProfile(username);
+  const user = await getUser(username);
 
   if (!user) {
     notFound();
@@ -25,4 +30,19 @@ export default async function UserPage({ params: { username } }: Props) {
       <UserPosts user={user} />
     </section>
   );
+}
+
+export async function generateMetadata({
+  params: { username }
+}: Props): Promise<Metadata> {
+  const user = await getUser(username);
+
+  if (!user) {
+    notFound();
+  }
+
+  return {
+    title: `${user?.name} (@${user?.username}) ∙ Vickygram`,
+    description: `${user?.name}'s all Vickygram posts`
+  };
 }
