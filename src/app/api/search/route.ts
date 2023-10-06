c5d5922f8fd4b1241f2296773cba01f5fc22c0d3
wonieeVicky <hwfongfing@gmail.com>
2023-10-06 22:38:04 +0900

    feat: api/search, search 페이지 SSG > SSR 페이지로 동작하도록 dynamic 변수 선언

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,7 +1,10 @@
 ﻿import { searchUsers } from '@/service/user';
 import { NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic'; // next에게 항상 요청을 동적으로 처리하도록 지시
+
 export async function GET() {
   // 로그인 여부를 보지 않는다.
+  // 항상 동일한 함수를 호출하므로 SSG로 처리됨
   return searchUsers().then((data) => NextResponse.json(data));
 }
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,6 +1,8 @@
 ﻿import UserSearch from '@/components/ui/UserSearch';
 import { Metadata } from 'next';
 
+export const dynamic = 'force-dynamic'; // next에게 항상 요청을 동적으로 처리하도록 지시
+
 export const metadata: Metadata = {
   title: 'User Search',
   description: 'Search users to follow'
