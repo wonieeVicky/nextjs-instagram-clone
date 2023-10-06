e98bd3b394f73261a4f19f832e46db3ded6fd7ba
wonieeVicky <hwfongfing@gmail.com>
2023-10-06 22:22:33 +0900

    feat: layout, signin, search page metadata 추가

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,5 +1,6 @@
 ﻿import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 import Signin from '@/components/layout/Signin';
+import { Metadata } from 'next';
 import { getServerSession } from 'next-auth';
 import { getProviders } from 'next-auth/react';
 import { redirect } from 'next/navigation';
@@ -8,6 +9,11 @@ type Props = {
   searchParams: {
     callbackUrl: string;
   };
+};
+
+export const metadata: Metadata = {
+  title: 'Signin',
+  description: 'Signup or Login to Vickygram'
 };
 
 export default async function SignInPage({
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,4 +1,10 @@
 ﻿import UserSearch from '@/components/ui/UserSearch';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'User Search',
+  description: 'Search users to follow'
+};
 
 export default function SearchPage() {
   return <UserSearch />;
