5b1b65e10bb996d558920e561b646e1393e2383b
wonieeVicky <hwfongfing@gmail.com>
2023-10-08 21:51:16 +0900

    feat: ActionBar 내 ToggleButton 컴포넌트 적용

--- src/components/ui/ActionBar.tsx
+++ src/components/ui/ActionBar.tsx
@@ -2,23 +2,43 @@
 import HeartFillIcon from '../ui/icons/HeartFillIcon';
 import BookmarkIcon from '../ui/icons/BookmarkIcon';
 import { parseDate } from '@/util/date';
-import { useSession } from 'next-auth/react';
+import { useState } from 'react';
+import ToggleButton from './ToggleButton';
+import BookmarkFillIcon from './icons/BookmarkFillIcon';
 
 type Props = {
   likes: string[];
   username: string;
   text?: string;
   createdAt: string;
+  id: string;
 };
 
-export default function ActionBar({ likes, username, text, createdAt }: Props) {
-  const { data: session } = useSession();
-  const likedPost = likes && likes.includes(session?.user.username || '');
+export default function ActionBar({
+  likes,
+  username,
+  text,
+  createdAt,
+  id
+}: Props) {
+  const [liked, setLiked] = useState(false);
+  const [bookmarked, setBookmarked] = useState(false);
+
   return (
     <>
       <div className="flex justify-between my-2 px-4">
-        <div>{likedPost ? <HeartFillIcon /> : <HeartIcon />}</div>
-        <BookmarkIcon />
+        <ToggleButton
+          toggled={liked}
+          onToggle={setLiked}
+          onIcon={<HeartFillIcon />}
+          offIcon={<HeartIcon />}
+        />
+        <ToggleButton
+          toggled={bookmarked}
+          onToggle={setBookmarked}
+          onIcon={<BookmarkFillIcon />}
+          offIcon={<BookmarkIcon />}
+        />
       </div>
       <div className="px-4 py-1">
         <p className="text-sm font-bold mb-2">{`${likes?.length ?? 0} ${
