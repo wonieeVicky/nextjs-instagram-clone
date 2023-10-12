e5a20b4333f37b7b20cc005b113205add4ad5d58
wonieeVicky <hwfongfing@gmail.com>
2023-10-12 23:30:57 +0900

    feat: usePosts custom hook 구현

---
+++ src/hooks/posts.ts
@@ -0,0 +1,15 @@
+﻿import { SimplePost } from '@/model/post';
+import useSWR, { useSWRConfig } from 'swr';
+
+// encapsulate the logic of posts
+export default function usePosts() {
+  const { data: posts, isLoading, error } = useSWR<SimplePost[]>('/api/posts');
+  const { mutate } = useSWRConfig();
+  const setLike = (post: SimplePost, username: string, like: boolean) =>
+    fetch('api/likes', {
+      method: 'PUT',
+      body: JSON.stringify({ id: post.id, like })
+    }).then(() => mutate('/api/posts'));
+
+  return { posts, isLoading, error, setLike };
+}
