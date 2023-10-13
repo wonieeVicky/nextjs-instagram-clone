173f9c316ae107756b880ada0c093cd8229442d2
wonieeVicky <hwfongfing@gmail.com>
2023-10-13 23:19:40 +0900

    refactor: 내부 mutate 로직 사용, revalidate 시 optimistic data를 사용하도록 리팩토링, updateLike 함수 별도 분리

--- src/hooks/posts.ts
+++ src/hooks/posts.ts
@@ -1,15 +1,40 @@
 ﻿import { SimplePost } from '@/model/post';
 import useSWR, { useSWRConfig } from 'swr';
 
+async function updateLike(id: string, like: boolean) {
+  return fetch('api/likes', {
+    method: 'PUT',
+    body: JSON.stringify({ id, like })
+  }).then((res) => res.json());
+}
+
 // encapsulate the logic of posts
 export default function usePosts() {
-  const { data: posts, isLoading, error } = useSWR<SimplePost[]>('/api/posts');
-  const { mutate } = useSWRConfig();
-  const setLike = (post: SimplePost, username: string, like: boolean) =>
-    fetch('api/likes', {
-      method: 'PUT',
-      body: JSON.stringify({ id: post.id, like })
-    }).then(() => mutate('/api/posts'));
+  const {
+    data: posts,
+    isLoading,
+    error,
+    mutate
+  } = useSWR<SimplePost[]>('/api/posts');
+
+  const setLike = (post: SimplePost, username: string, like: boolean) => {
+    // optimistic update를 위한 newPost 생성
+    const newPost = {
+      ...post,
+      likes: like
+        ? [...post.likes, username]
+        : post.likes.filter((u) => u !== username)
+    };
+    const newPosts = posts?.map((p) => (p.id === post.id ? newPost : p));
+
+    // 내부 mutate를 사용해서 optimistic update 구현
+    return mutate(updateLike(post.id, like), {
+      optimisticData: newPosts,
+      populateCache: false,
+      revalidate: true,
+      rollbackOnError: true
+    });
+  };
 
   return { posts, isLoading, error, setLike };
 }
