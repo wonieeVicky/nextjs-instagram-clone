7463ea28e55bd683144fd3ea8a9f0dab49846728
wonieeVicky <hwfongfing@gmail.com>
2023-10-16 00:02:10 +0900

    fix: getSavedPostOf service 함수 오타 수정

--- src/service/posts.ts
+++ src/service/posts.ts
@@ -61,7 +61,7 @@ export async function getLikedPostsOf(username: string) {
 export async function getSavedPostsOf(username: string) {
   return client
     .fetch(
-      `*[_type == "post" && _id in *[_type == "user" && username == "${username}".bookmarks[]._ref]] | order(_createdAt desc) {${simplePostProjection}}`
+      `*[_type == "post" && _id in *[_type == "user" && username == "${username}"].bookmarks[]._ref] | order(_createdAt desc) {${simplePostProjection}}`
     )
     .then(mapPosts);
 }
