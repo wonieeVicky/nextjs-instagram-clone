3f5489a0b379311bcdd81013d3fee1cb7fb5351b
wonieeVicky <hwfongfing@gmail.com>
2023-10-20 22:52:20 +0900

    feat: post hooks 제거, posts 내에서 postComment 함수 추가

--- src/hooks/posts.ts
+++ src/hooks/posts.ts
@@ -7,6 +7,12 @@ async function updateLike(id: string, like: boolean) {
     body: JSON.stringify({ id, like })
   }).then((res) => res.json());
 }
+async function addComment(id: string, comment: string) {
+  return fetch('api/comments', {
+    method: 'POST',
+    body: JSON.stringify({ id, comment })
+  }).then((res) => res.json());
+}
 
 // encapsulate the logic of posts
 export default function usePosts() {
@@ -36,5 +42,22 @@ export default function usePosts() {
     });
   };
 
-  return { posts, isLoading, error, setLike };
+  const postComment = (post: SimplePost, comment: string) => {
+    // optimistic update를 위한 newPost 생성
+    const newPost = {
+      ...post,
+      comments: post.comments + 1
+    };
+    const newPosts = posts?.map((p) => (p.id === post.id ? newPost : p));
+
+    // 내부 mutate를 사용해서 optimistic update 구현
+    return mutate(addComment(post.id, comment), {
+      optimisticData: newPosts,
+      populateCache: false,
+      revalidate: false, // 재갱신 x, newPosts 데이터 신뢰, 네트워크 통신 절약
+      rollbackOnError: true
+    });
+  };
+
+  return { posts, isLoading, error, setLike, postComment };
 }
