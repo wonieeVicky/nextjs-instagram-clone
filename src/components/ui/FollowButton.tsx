79ac2e3541d3b7b25a3fbc3660375fddc7b88205
wonieeVicky <hwfongfing@gmail.com>
2023-10-27 16:10:37 +0900

    feat: FollowButton 내 setFollow 실행

--- src/components/ui/FollowButton.tsx
+++ src/components/ui/FollowButton.tsx
@@ -8,19 +8,21 @@ type Props = {
   user: ProfileUser;
 };
 export default function FollowButton({ user }: Props) {
-  const { username } = user;
-  const { user: loggedInUser } = useMe();
+  const { username, image } = user;
+  const { user: loggedInUser, setFollow } = useMe();
   const showButton = loggedInUser && loggedInUser.username !== username;
   const following =
     loggedInUser &&
     loggedInUser.following.find((item) => item.username === username);
 
   const text = following ? 'Unfollow' : 'Follow';
+  const updateFollow = () =>
+    setFollow(username, image, following ? true : false);
 
   return (
     <>
       {showButton && (
-        <Button text={text} onClick={() => {}} red={text === 'Unfollow'} />
+        <Button text={text} onClick={updateFollow} red={text === 'Unfollow'} />
       )}
     </>
   );
