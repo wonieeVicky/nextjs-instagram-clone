016f044c63274e7d0c97c27c3a75e07c82c842d2
wonieeVicky <hwfongfing@gmail.com>
2023-10-27 16:10:14 +0900

    feat: useMe 내 setFollow 유틸함수 추가

--- src/hooks/me.ts
+++ src/hooks/me.ts
@@ -9,9 +9,38 @@ async function updateBookmark(postId: string, bookmark: boolean) {
   }).then((res) => res.json());
 }
 
+async function updateFollowing(userId: string, follow: boolean) {
+  return fetch('api/follow', {
+    method: 'PUT',
+    body: JSON.stringify({ id: userId, follow })
+  }).then((res) => res.json());
+}
+
 // encapsulate the logic of posts
 export default function useMe() {
   const { data: user, isLoading, error, mutate } = useSWR<HomeUser>('/api/me');
+
+  const setFollow = useCallback(
+    (userId: string, image: string | undefined, follow: boolean) => {
+      if (!user) return;
+      const following = user.following;
+
+      const newUser = {
+        ...user,
+        following: follow
+          ? [...following, { image, username: userId }]
+          : following.filter((u) => u.username !== userId)
+      };
+
+      return mutate(updateFollowing(userId, follow), {
+        optimisticData: newUser,
+        populateCache: false,
+        revalidate: false, // 재갱신 x, newPosts 데이터 신뢰, 네트워크 통신 절약
+        rollbackOnError: true
+      });
+    },
+    [user, mutate]
+  );
 
   const setBookmark = useCallback(
     (postId: string, bookmark: boolean) => {
@@ -35,5 +64,5 @@ export default function useMe() {
     [user, mutate]
   );
 
-  return { user, isLoading, error, setBookmark };
+  return { user, isLoading, error, setBookmark, setFollow };
 }
