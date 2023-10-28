82ea1ca53e8b5a337c404bdb669901a0eb3c215c
wonieeVicky <hwfongfing@gmail.com>
2023-10-28 23:33:32 +0900

    chore: follow, unfollow 함수 변경에 따른 service api update

--- src/app/api/follow/route.ts
+++ src/app/api/follow/route.ts
@@ -1,7 +1,7 @@
 ﻿import { getServerSession } from 'next-auth';
 import { NextRequest, NextResponse } from 'next/server';
 import { authOptions } from '@/app/api/auth/[...nextauth]/route';
-import { followUser, unfollowUser } from '@/service/user';
+import { follow, unfollow } from '@/service/user';
 
 // PUT Follow
 export async function PUT(req: NextRequest) {
@@ -12,14 +12,15 @@ export async function PUT(req: NextRequest) {
     return new Response('Authentication Error', { status: 401 });
   }
 
-  const { id, follow } = await req.json();
+  const { id: targetId, follow: isFollow } = await req.json();
 
-  if (!id || follow === undefined) {
+  if (!targetId || follow === undefined) {
     return new Response('Bad Request', { status: 400 });
   }
 
-  const request = follow ? followUser : unfollowUser;
-  return request(user.id, id)
+  const request = isFollow ? follow : unfollow;
+
+  return request(user.id, targetId)
     .then((res) => NextResponse.json(res))
     .catch((error) => new Response(JSON.stringify(error), { status: 500 }));
 }
