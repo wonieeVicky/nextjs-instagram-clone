f45593229a6a6cc3a04c363658624f76cd3bb0c9
wonieeVicky <hwfongfing@gmail.com>
2023-10-31 23:24:23 +0900

    feat: usePosts에서 사용하는 CacheKeysContext 생성

---
+++ src/context/CacheKeysContext.tsx
@@ -0,0 +1,11 @@
+﻿import { createContext, useContext } from 'react';
+
+type CacheKeysValue = {
+  postsKey: string;
+};
+
+export const CacheKeysContext = createContext<CacheKeysValue>({
+  postsKey: '/api/posts'
+});
+
+export const useCacheKeys = () => useContext(CacheKeysContext);
