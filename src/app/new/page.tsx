1cae022ef33bc04750462d16ddaaf8c9b98aba12
wonieeVicky <hwfongfing@gmail.com>
2023-11-01 18:18:58 +0900

    feat: new 페이지 내 CreatePost 컴포넌트 추가, 기본 레이아웃 구성

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,3 +1,27 @@
-﻿export default function SearchPage() {
-  return <>New Post Page</>;
+﻿import { getServerSession } from 'next-auth';
+import { authOptions } from '../api/auth/[...nextauth]/route';
+import Avatar from '@/components/ui/Avatar';
+import { redirect } from 'next/navigation';
+import CreatePost from '@/components/ui/CreatePost';
+
+export default async function SearchPage() {
+  const session = await getServerSession(authOptions);
+
+  if (!session?.user) {
+    redirect('/auth/signin');
+  }
+
+  const { image, username } = session?.user;
+
+  return (
+    <section className="w-full">
+      <div className="flex items-center justify-center my-5">
+        {image && <Avatar image={image} highlight size="medium" />}
+        <div className="ml-3">
+          <p className="font-bold">{username}</p>
+        </div>
+      </div>
+      <CreatePost />
+    </section>
+  );
 }
