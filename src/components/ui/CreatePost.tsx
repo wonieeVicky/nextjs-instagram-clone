b2b2952e2b36a911a2660454d4a3628196a7e4d5
wonieeVicky <hwfongfing@gmail.com>
2023-11-01 18:18:25 +0900

    feat: CreatePost 기본 컴포넌트 생성 및 upload ui 구현, UploadIcon 자산 추가

---
+++ src/components/ui/CreatePost.tsx
@@ -0,0 +1,35 @@
+﻿'use client';
+
+import UploadIcon from './icons/UploadIcon';
+
+export default function CreatePost() {
+  const uploadImage = (e) => {
+    console.log(e);
+  };
+  return (
+    <>
+      <div className="w-8/12 m-auto">
+        <div className="flex items-center justify-center w-full">
+          <label className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50">
+            <div className="flex flex-col items-center justify-center pt-5 pb-6">
+              <UploadIcon />
+              <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                <span className="font-semibold">Click to upload</span> or drag
+                and drop
+              </p>
+              <p className="text-xs text-gray-500 dark:text-gray-400">
+                SVG, PNG, JPG or GIF (MAX. 800x400px)
+              </p>
+            </div>
+            <input
+              id="dropzone-file"
+              type="file"
+              className="hidden"
+              onChange={uploadImage}
+            />
+          </label>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/components/ui/icons/UploadIcon.tsx
@@ -0,0 +1,5 @@
+﻿import { AiOutlineCloudUpload } from 'react-icons/ai';
+
+export default function UploadIcon() {
+  return <AiOutlineCloudUpload className="w-24 h-24 text-slate-400" />;
+}
