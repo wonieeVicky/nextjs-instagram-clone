5bcb43fad8e199aeaadd0f919d3e639f19465b0b
wonieeVicky <hwfongfing@gmail.com>
2023-11-02 23:28:45 +0900

    feat: Button 컴포넌트에 block 옵션 속성 추가

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -3,19 +3,21 @@
   onClick: () => void;
   red?: boolean;
   disabled?: boolean;
+  block?: boolean;
 };
 
 export default function Button({
   text,
   onClick,
   red,
-  disabled = false
+  disabled = false,
+  block
 }: Props) {
   return (
     <button
       className={`border-none rounded-md py-2 px-8 text-white font-bold leading-4 ${
         red ? 'bg-red-500' : 'bg-sky-500'
-      } ${disabled && 'opacity-50 cursor-not-allowed'}`}
+      } ${disabled && 'opacity-50 cursor-not-allowed'} ${block && 'w-full'}`}
       onClick={onClick}
       disabled={disabled}
     >
