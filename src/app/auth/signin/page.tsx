55a4c530cb9c2a39302be5188a15661f72c9f89a
wonieeVicky <hwfongfing@gmail.com>
2023-11-12 23:16:05 +0900

    chore: test code(build test)

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -29,6 +29,7 @@ export default async function SignPage({
 
   return (
     <section className="flex justify-center mt-24">
+      testtest
       <Signin providers={providers} callbackUrl={callbackUrl ?? '/'} />
     </section>
   );
